787690e81115f5969b45e393847202774053613f
Rachit Agarwal <rachitrajeshagarwal8484@gmail.com>
2022-06-28 18:49:44 -0700

    Players deployed!

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,15 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Rach", "Matt", "Pika"], // Names
+    [
+      "https://twitter.com/rachit8484/photo", // Images
+      "https://twitter.com/tamgros/photo",
+      "https://i.imgur.com/WMB6g9u.png",
+    ],
+    [100, 200, 300], // HP values
+    [150, 500, 25] // Attack damage values
+  );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 };
